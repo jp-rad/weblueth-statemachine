28c22912f71b78e8ef8526f5e2cf3ca89f2228f5
jp-rad <69703808+jp-rad@users.noreply.github.com>
2023-03-18 23:58:24 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# @weblueth/custom
+
+@weblueth, custom template.
+
+[Live Demo (GitHub Pages)](https://jp-rad.github.io/weblueth-custom/)
+
+## Installation:
+
+```bash
+npm install @weblueth/custom --save-dev
+```
+
+or
+
+```bash
+yarn add --dev @weblueth/custom
+```
---
+++ pages/app/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Logo from '../../src';  // logo.svg ==> Logo.tsx
+//import './App.css'; // ==> ../index.html
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Logo className="App-logo" />
+        <p>
+          Edit <code>src/app/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
